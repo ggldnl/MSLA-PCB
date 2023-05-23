2245e83aa16caf50684896d13cc4fd1b80f1deba
ggldnl <danielgigliotti99.dg@gmail.com>
2023-05-23 09:19:16 +0200

    stripped pdf related part

--- png2stl.py
+++ png2stl.py
@@ -2,7 +2,6 @@ from pathlib import PurePath
 from skimage import measure
 from stl import mesh
 import numpy as np
-import pdf2image
 import argparse
 import cv2
 import os
@@ -18,48 +17,6 @@ def get_local_folder():
     except NameError:
         pass
     return os.path.abspath("")  # ipynb
-
-
-def pdf_2_np(filepath, dpi=400):  # 400 is a good trade-off
-    """
-    Given a single page pdf, convert it to image and return it as a numpy array.
-
-    Attributes
-    ----------
-    filepath : str
-        input pdf path
-
-    dpi: int
-        Dots Per Inch. By default, pdf2Image uses resolution of 92 DPI, 
-        which is the typical screen resolution. Smaller DPI numbers result in 
-        smaller images (e.g. suitable for use as thumbnails), while larger DPI 
-        numbers generate larger images (e.g. suitable for high-quality output)
-    
-    Returns
-    -------
-    img: np.array
-        Numpy array containing the single pdf page converted as image
-
-    Throws
-    ------
-    ValueError
-        if the pdf contains a number of pages different than the expected 1
-    
-    IOError
-        for IO problems with the file
-    """
-
-    # the pdf contains only one page
-    images = pdf2image.convert_from_path(filepath, dpi=dpi)
-
-    if images is None:  # test if image exists and is read successfully
-        raise IOError(f'Cannot find file: {str(filepath)}')
-    
-    if len(images) != 1:
-        raise ValueError(f'Expected a single page pdf, got {len(images)} pages instead.')    
-
-    # get the image data as array
-    return np.asarray(images[0])
 
 
 def _unpad(x, pad_width):
@@ -240,27 +197,9 @@ if __name__ == '__main__':
     # ----------------------------------- main ----------------------------------- #
 
     stem = input_path.stem
-    suffix = input_path.suffix.lower()
     folder = input_path.parent
 
-    if suffix.endswith('.pdf'):
-        # Some softwares (e.g. easyEDA) let you export the pcb as a pdf file;
-        # sometimes this is preferable since we can convert it to an image
-        # specifying the DPIs, thus increasing the quality before conversion.
-        #
-        # Read the pdf file, get an image from it and save it in the same
-        # folder, with the same name (except of course the extension)
-        img = pdf_2_np(input_path, dpi=400)
-        
-        image_path = PurePath(folder, stem + '.png')
-        cv2.imwrite(str(image_path), img)
-
-    elif suffix.endswith('.png'):
-        
-        img = cv2.imread(str(input_path))
-
-    else:
-        raise ValueError(f'Invalid input file: {input_path}')
+    img = cv2.imread(str(input_path))
 
     # we have the image as a numpy array, remove external padding
     # and apply other preprocessing steps
